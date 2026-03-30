1b020ba178d7286993e9829335976a4acf6efd8d
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-03-30 02:08:14 +0200

    bug fix?

--- s88/ControlModule/network/api/container-selection.nix
+++ s88/ControlModule/network/api/container-selection.nix
@@ -8,6 +8,7 @@ let
   sortedAttrNames = attrs: lib.sort builtins.lessThan (builtins.attrNames attrs);
 
   selectionNames = sortedAttrNames containerSelection;
+  containerNames = sortedAttrNames containers;
 
   _validateSelectionValues = builtins.foldl' (
     acc: name:
@@ -25,18 +26,73 @@ let
       ''
   ) true selectionNames;
 
-  containerNames = sortedAttrNames containers;
+  aliasesForContainer =
+    containerName:
+    let
+      container = containers.${containerName};
+      unitName =
+        if
+          container ? specialArgs
+          && builtins.isAttrs container.specialArgs
+          && container.specialArgs ? unitName
+          && builtins.isString container.specialArgs.unitName
+        then
+          container.specialArgs.unitName
+        else
+          null;
+    in
+    lib.unique (
+      lib.filter builtins.isString [
+        containerName
+        unitName
+      ]
+    );
 
-  enabledNames = lib.filter (name: containerSelection.${name}) selectionNames;
-  disabledNames = lib.filter (name: !containerSelection.${name}) selectionNames;
+  aliasMatchesSelector =
+    selector: alias:
+    alias == selector || lib.hasSuffix "-${selector}" alias || lib.hasSuffix "::${selector}" alias;
+
+  containerMatchesSelector =
+    selector: containerName:
+    lib.any (alias: aliasMatchesSelector selector alias) (aliasesForContainer containerName);
+
+  resolveSelector =
+    selector:
+    let
+      matches = lib.filter (
+        containerName: containerMatchesSelector selector containerName
+      ) containerNames;
+    in
+    if matches == [ ] then
+      throw ''
+        s88/ControlModule/network/api/container-selection.nix: selector '${selector}' did not match any rendered container
+
+        available container names:
+        ${builtins.toJSON containerNames}
+      ''
+    else if builtins.length matches == 1 then
+      builtins.head matches
+    else
+      throw ''
+        s88/ControlModule/network/api/container-selection.nix: selector '${selector}' matched multiple rendered containers
+
+        matches:
+        ${builtins.toJSON matches}
+      '';
+
+  enabledSelectors = lib.filter (name: containerSelection.${name}) selectionNames;
+  disabledSelectors = lib.filter (name: !containerSelection.${name}) selectionNames;
+
+  enabledContainerNames = map resolveSelector enabledSelectors;
+  disabledContainerNames = map resolveSelector disabledSelectors;
 
   selectedNames =
     if containerSelection == { } then
       containerNames
-    else if enabledNames != [ ] then
-      lib.filter (name: builtins.elem name enabledNames) containerNames
     else
-      lib.filter (name: !(builtins.elem name disabledNames)) containerNames;
+      lib.filter (name: !(builtins.elem name disabledContainerNames)) (
+        if enabledSelectors != [ ] then enabledContainerNames else containerNames
+      );
 in
 builtins.seq _validateSelectionValues (
   builtins.listToAttrs (
