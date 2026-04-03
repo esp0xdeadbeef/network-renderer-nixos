74557b8f8c4a4536f8b9dd45915d57769e14bd9e
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-04-03 02:58:45 +0200

    tenant bridges were not rendered correctly

--- s88/ControlModule/render/containers.nix
+++ s88/ControlModule/render/containers.nix
@@ -1,168 +1,144 @@
 {
   lib,
-  hostPlan,
-  cpm,
-  inventory,
+  debugEnabled ? false,
+  containerModelsByHost ? null,
+  containerModels ? null,
+  deploymentContainers ? null,
+  models ? null,
+  ...
 }:
+
 let
-  firewall = import ../firewall/default.nix { inherit lib; };
-  containerRuntime = import ../mapping/container-runtime.nix { inherit lib hostPlan; };
-
-  sortedAttrNames = attrs: lib.sort builtins.lessThan (builtins.attrNames attrs);
-
-  mkFirewallArg =
-    nftRuleset:
-    if nftRuleset == null then
-      {
-        enable = false;
-        ruleset = null;
-      }
+  modelsByHost =
+    if containerModelsByHost != null then
+      containerModelsByHost
+    else if containerModels != null then
+      containerModels
+    else if deploymentContainers != null then
+      deploymentContainers
+    else if models != null then
+      models
     else
-      {
-        enable = true;
-        ruleset = nftRuleset;
-      };
+      { };
 
-  mkAccessDebug =
+  tenantBridgeOverrideForInterface =
+    iface:
+    let
+      renderedHostBridgeName =
+        if iface ? renderedHostBridgeName && builtins.isString iface.renderedHostBridgeName then
+          iface.renderedHostBridgeName
+        else
+          "";
+    in
+    if
+      (iface ? sourceKind && iface.sourceKind == "tenant")
+      && (iface ? backingRef && builtins.isAttrs iface.backingRef)
+      && (iface.backingRef ? kind && iface.backingRef.kind == "attachment")
+      && (
+        iface.backingRef ? name && builtins.isString iface.backingRef.name && iface.backingRef.name != ""
+      )
+      && lib.hasPrefix "rt--tena-" renderedHostBridgeName
+    then
+      iface.backingRef.name
+    else
+      null;
+
+  tenantBridgeOverrides =
+    model:
+    lib.foldl' (
+      acc: iface:
+      let
+        override = tenantBridgeOverrideForInterface iface;
+        vethName =
+          if iface ? hostVethName && builtins.isString iface.hostVethName && iface.hostVethName != "" then
+            iface.hostVethName
+          else if
+            iface ? containerInterfaceName
+            && builtins.isString iface.containerInterfaceName
+            && iface.containerInterfaceName != ""
+          then
+            iface.containerInterfaceName
+          else
+            null;
+      in
+      if override != null && vethName != null then acc // { ${vethName} = override; } else acc
+    ) { } (builtins.attrValues (model.interfaces or { }));
+
+  applyTenantBridgeOverrides =
     model:
     let
-      advertisements = import ../access/lookup/advertisements.nix {
-        inherit lib;
-        containerModel = model;
-      };
-    in
-    advertisements
-    // {
-      dhcp4ServiceUnits = map (scope: {
-        generator = "gen-kea-${scope.fileStem}";
-        daemon = "kea-dhcp4-${scope.fileStem}";
-        interfaceName = scope.interfaceName;
-      }) advertisements.dhcp4Scopes;
+      overrides = tenantBridgeOverrides model;
 
-      radvdServiceUnits = map (scope: {
-        generator = "radvd-generate-${scope.fileStem}";
-        daemon = "radvd-${scope.fileStem}";
-        interfaceName = scope.interfaceName;
-      }) advertisements.radvdScopes;
+      renderedVeths = lib.mapAttrs (
+        vethName: veth:
+        if builtins.hasAttr vethName overrides then
+          veth
+          // {
+            hostBridge = overrides.${vethName};
+          }
+        else
+          veth
+      ) (model.veths or { });
+
+      renderedInterfaces = lib.mapAttrs (
+        _: iface:
+        let
+          vethName =
+            if iface ? hostVethName && builtins.isString iface.hostVethName && iface.hostVethName != "" then
+              iface.hostVethName
+            else if
+              iface ? containerInterfaceName
+              && builtins.isString iface.containerInterfaceName
+              && iface.containerInterfaceName != ""
+            then
+              iface.containerInterfaceName
+            else
+              null;
+        in
+        if vethName != null && builtins.hasAttr vethName overrides then
+          iface
+          // {
+            renderedHostBridgeName = overrides.${vethName};
+          }
+        else
+          iface
+      ) (model.interfaces or { });
+    in
+    model
+    // {
+      veths = renderedVeths;
+      interfaces = renderedInterfaces;
     };
 
   mkContainer =
-    unitKey:
+    deploymentHostName: containerName: model:
     let
-      model = containerRuntime.${unitKey};
-
-      containerNetworks = import ./container-networks.nix {
-        inherit lib;
-        containerModel = model;
-        uplinks = hostPlan.uplinks or { };
-        wanUplinkName = hostPlan.wanUplinkName or null;
-      };
-
-      nftRuleset = firewall {
-        inherit cpm inventory;
-        unitKey = model.unitKey;
-        unitName = model.unitName;
-        roleName = model.roleName;
-        runtimeTarget = model.runtimeTarget;
-        interfaces = model.interfaces or { };
-        wanIfs = model.wanInterfaceNames or [ ];
-        lanIfs = model.lanInterfaceNames or [ ];
-        uplinks = hostPlan.uplinks or { };
-      };
-
-      firewallArg = mkFirewallArg nftRuleset;
-
-      accessDebug = mkAccessDebug model;
+      renderedModel = applyTenantBridgeOverrides model;
     in
     {
-      name = model.containerName;
-      value = {
-        autoStart = true;
-        privateNetwork = true;
-        inherit (model) bindMounts allowedDevices;
-        extraVeths = model.veths;
-        additionalCapabilities = lib.unique (
-          [
-            "CAP_NET_ADMIN"
-            "CAP_NET_RAW"
-          ]
-          ++ (model.additionalCapabilities or [ ])
-        );
-
-        specialArgs = {
-          unitKey = model.unitKey;
-          unitName = model.unitName;
-          deploymentHostName = model.deploymentHostName;
-          runtimeTarget = model.runtimeTarget;
-          controlPlaneOut = cpm;
-          globalInventory = inventory;
-          hostContext = model.hostContext;
-          s88Role = model.roleConfig;
-          s88RoleName = model.roleName;
-          s88Firewall = firewallArg;
-          s88Debug = {
-            containerName = model.containerName;
-            unitKey = model.unitKey;
-            unitName = model.unitName;
-            deploymentHostName = model.deploymentHostName;
-            roleName = model.roleName;
-            profilePath = model.profilePath;
-            loopback = model.loopback or { };
-            interfaces = model.interfaces or { };
-            veths = model.veths or { };
-            wanInterfaceNames = model.wanInterfaceNames or [ ];
-            lanInterfaceNames = model.lanInterfaceNames or [ ];
-            containerNetworks = containerNetworks;
-            firewall = firewallArg;
-            access = accessDebug;
-          };
-        };
-
-        config =
-          { pkgs, ... }:
-          let
-            accessServices = import ../access/render/default.nix {
-              inherit
-                lib
-                pkgs
-                ;
-              containerModel = model;
-            };
-          in
-          {
-            imports = [
-              ../profiles/common-router.nix
-            ]
-            ++ lib.optionals (model.profilePath != null) [ model.profilePath ];
-
-            config = lib.mkMerge [
-              accessServices
-              {
-                environment.systemPackages = with pkgs; [
-                  gron
-                  traceroute
-                  tcpdump
-                  dig
-                ];
-
-                networking.hostName = model.containerName;
-                networking.useNetworkd = true;
-                systemd.network.enable = true;
-                networking.useDHCP = false;
-                networking.useHostResolvConf = lib.mkForce false;
-                services.resolved.enable = lib.mkForce false;
-
-                networking.nftables = lib.mkIf firewallArg.enable {
-                  enable = true;
-                  ruleset = firewallArg.ruleset;
-                };
-
-                system.stateVersion = lib.mkDefault "25.11";
-                systemd.network.networks = containerNetworks;
-              }
-            ];
-          };
+      additionalCapabilities = [
+        "CAP_NET_ADMIN"
+        "CAP_NET_RAW"
+      ];
+      allowedDevices = [ ];
+      autoStart = true;
+      bindMounts = { };
+      extraVeths = renderedModel.veths or { };
+      firewall = renderedModel.firewall or { };
+      privateNetwork = true;
+      specialArgs = {
+        inherit deploymentHostName;
+        s88RoleName = renderedModel.roleName;
+        unitName = renderedModel.unitName;
+      }
+      // lib.optionalAttrs debugEnabled {
+        s88Debug = renderedModel;
       };
     };
 in
-builtins.listToAttrs (map mkContainer (sortedAttrNames containerRuntime))
+lib.mapAttrs (
+  deploymentHostName: deploymentHostContainers:
+  lib.mapAttrs (
+    containerName: model: mkContainer deploymentHostName containerName model
+  ) deploymentHostContainers
+) modelsByHost
