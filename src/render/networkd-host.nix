12cd877ad4041a614a5e116c25a97f82d5cce620
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-04-18 21:49:53 +0200

    restore host uplinks to networkd and remove vm networking split path

--- src/render/networkd-host.nix
+++ src/render/networkd-host.nix
@@ -3,9 +3,120 @@ hostModel:
 let
   uplinkNames = builtins.attrNames hostModel.uplinks;
 
-  netdevs = { };
+  keepaliveNameFor = bridgeName: "ka-${bridgeName}";
 
-  networkingVlans = builtins.listToAttrs (
+  netdevs = builtins.listToAttrs (
+    lib.concatMap (
+      uplinkName:
+      let
+        uplink = hostModel.uplinks.${uplinkName};
+        keepaliveName = keepaliveNameFor uplink.bridgeName;
+      in
+      if uplink.kind == "vlan-bridge" then
+        [
+          {
+            name = uplink.vlanInterfaceName;
+            value = {
+              netdevConfig = {
+                Kind = "vlan";
+                Name = uplink.vlanInterfaceName;
+              };
+              vlanConfig = {
+                Id = uplink.vlanId;
+              };
+            };
+          }
+          {
+            name = uplink.bridgeName;
+            value = {
+              netdevConfig = {
+                Kind = "bridge";
+                Name = uplink.bridgeName;
+              };
+            };
+          }
+          {
+            name = keepaliveName;
+            value = {
+              netdevConfig = {
+                Kind = "dummy";
+                Name = keepaliveName;
+              };
+            };
+          }
+        ]
+      else
+        [
+          {
+            name = uplink.bridgeName;
+            value = {
+              netdevConfig = {
+                Kind = "bridge";
+                Name = uplink.bridgeName;
+              };
+            };
+          }
+          {
+            name = keepaliveName;
+            value = {
+              netdevConfig = {
+                Kind = "dummy";
+                Name = keepaliveName;
+              };
+            };
+          }
+        ]
+    ) uplinkNames
+  );
+
+  parentNetworks = builtins.listToAttrs (
+    map (
+      uplinkName:
+      let
+        uplink = hostModel.uplinks.${uplinkName};
+      in
+      {
+        name = "10-${uplink.parent}-${uplinkName}";
+        value =
+          if uplink.kind == "vlan-bridge" then
+            {
+              matchConfig.Name = uplink.parent;
+              networkConfig.VLAN = [ uplink.vlanInterfaceName ];
+            }
+          else
+            {
+              matchConfig.Name = uplink.parent;
+              networkConfig.Bridge = uplink.bridgeName;
+            };
+      }
+    ) uplinkNames
+  );
+
+  keepaliveNetworks = builtins.listToAttrs (
+    map (
+      uplinkName:
+      let
+        uplink = hostModel.uplinks.${uplinkName};
+        keepaliveName = keepaliveNameFor uplink.bridgeName;
+      in
+      {
+        name = "15-${keepaliveName}";
+        value = {
+          matchConfig.Name = keepaliveName;
+          networkConfig = {
+            Bridge = uplink.bridgeName;
+            ConfigureWithoutCarrier = true;
+          };
+          linkConfig = {
+            ActivationPolicy = "always-up";
+            RequiredForOnline = "no";
+          };
+        };
+      }
+    ) uplinkNames
+  );
+
+  childNetworks = builtins.listToAttrs (
     lib.concatMap (
       uplinkName:
       let
@@ -14,100 +125,54 @@ let
       if uplink.kind == "vlan-bridge" then
         [
           {
-            name = uplink.vlanInterfaceName;
+            name = "20-${uplink.vlanInterfaceName}";
             value = {
-              id = uplink.vlanId;
-              interface = uplink.parent;
+              matchConfig.Name = uplink.vlanInterfaceName;
+              networkConfig.Bridge = uplink.bridgeName;
+            };
+          }
+          {
+            name = "30-${uplink.bridgeName}";
+            value = {
+              matchConfig.Name = uplink.bridgeName;
+              networkConfig = uplink.networkOptions // {
+                ConfigureWithoutCarrier = true;
+              };
+              linkConfig = {
+                ActivationPolicy = "always-up";
+                RequiredForOnline = "no";
+              };
             };
           }
         ]
       else
-        [ ]
-    ) uplinkNames
-  );
-
-  networkingBridges = builtins.listToAttrs (
-    map (
-      uplinkName:
-      let
-        uplink = hostModel.uplinks.${uplinkName};
-      in
-      {
-        name = uplink.bridgeName;
-        value = {
-          interfaces =
-            if uplink.kind == "vlan-bridge" then [ uplink.vlanInterfaceName ] else [ uplink.parent ];
-        };
-      }
-    ) uplinkNames
-  );
-
-  renderedNetworks = builtins.listToAttrs (
-    map (
-      uplinkName:
-      let
-        uplink = hostModel.uplinks.${uplinkName};
-
-        dhcpValue =
-          if uplink.networkOptions ? DHCP then
-            uplink.networkOptions.DHCP
-          else if uplink.networkOptions ? dhcp then
-            uplink.networkOptions.dhcp
-          else
-            null;
-
-        ipv6AcceptRAValue =
-          if uplink.networkOptions ? IPv6AcceptRA then
-            uplink.networkOptions.IPv6AcceptRA
-          else if uplink.networkOptions ? ipv6AcceptRA then
-            uplink.networkOptions.ipv6AcceptRA
-          else
-            null;
-
-        passthroughNetworkOptions = builtins.removeAttrs uplink.networkOptions [
-          "DHCP"
-          "dhcp"
-          "IPv6AcceptRA"
-          "ipv6AcceptRA"
-        ];
-      in
-      {
-        name = "30-${uplink.bridgeName}";
-        value = {
-          matchConfig.Name = uplink.bridgeName;
-          linkConfig = {
-            ActivationPolicy = "always-up";
-            RequiredForOnline = "no";
-          };
-          networkConfig =
-            passthroughNetworkOptions
-            // (lib.optionalAttrs (dhcpValue != null) {
-              DHCP = dhcpValue;
-            })
-            // (lib.optionalAttrs (ipv6AcceptRAValue != null) {
-              IPv6AcceptRA = ipv6AcceptRAValue;
-            })
-            // {
-              ConfigureWithoutCarrier = true;
+        [
+          {
+            name = "20-${uplink.bridgeName}";
+            value = {
+              matchConfig.Name = uplink.bridgeName;
+              networkConfig = uplink.networkOptions // {
+                ConfigureWithoutCarrier = true;
+              };
+              linkConfig = {
+                ActivationPolicy = "always-up";
+                RequiredForOnline = "no";
+              };
             };
-        };
-      }
+          }
+        ]
     ) uplinkNames
   );
+
+  renderedNetworks = parentNetworks // keepaliveNetworks // childNetworks;
 in
 {
   hostName = hostModel.hostName;
   deploymentHostName = hostModel.deploymentHostName;
   netdevs = netdevs;
   networks = renderedNetworks;
-  networking = {
-    vlans = networkingVlans;
-    bridges = networkingBridges;
-  };
   debug = hostModel.debug // {
     renderedNetdevs = builtins.attrNames netdevs;
     renderedNetworks = builtins.attrNames renderedNetworks;
-    renderedVlans = builtins.attrNames networkingVlans;
-    renderedBridges = builtins.attrNames networkingBridges;
   };
 }
