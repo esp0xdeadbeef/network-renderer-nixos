73f653222d7cdcd455b9b6c783c18c1315fb20ae
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-04-23 22:52:59 +0200

    fix host validation shell quoting

--- s88/ControlModule/module/host-validation.nix
+++ s88/ControlModule/module/host-validation.nix
@@ -108,15 +108,15 @@ let
           --argjson dns_service "$dns_service" \
           --arg dns4 "$dns4" \
           --arg dns6 "$dns6" \
-          '{
+          "{
             systemState: $system_state,
             defaultRoute4: $default4,
             defaultRoute6: $default6,
             dnsService: $dns_service,
             dnsA: $dns4,
             dnsAAAA: $dns6
-          }'
-      2>/dev/null || jq -n '{ error: "check-failed" }'
+          }"
+      2>/dev/null || jq -n "{ error: \"check-failed\" }"
     }
 
     while true; do
