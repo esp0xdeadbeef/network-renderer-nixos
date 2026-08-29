828f200c4e186f0adeb7a51d3a2af9e70f25ee4e
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-08-29 16:04:02 +0200

    revert(render): drop incomplete multipath rule detection (FS-315)

--- s88/ControlModule/render/container-networks/policy-routing/aggregate.nix
+++ s88/ControlModule/render/container-networks/policy-routing/aggregate.nix
@@ -116,8 +116,6 @@ builtins.foldl'
           isUpstreamSelectorCoreInterface interfaceName
           && !(isPolicy || isDownstreamSelectorPolicyInterface interfaceName)
         );
-
-      isMultipathDownstream = true; # DEBUG-FORCE
       effectiveMainSourceScope = sourceScope // {
         staticPrefixes = lib.unique (sourceScope.staticPrefixes ++ forwardingMainScope.staticPrefixes);
         sourceFiles = lib.unique (sourceScope.sourceFiles ++ forwardingMainScope.sourceFiles);
@@ -228,7 +226,6 @@ builtins.foldl'
           sourceIfName == ifName
           && isReturnSideSelfIngress
           && !(isUpstreamSelectorCoreInterface interfaceName)
-          && !isMultipathDownstream
         then
           [ ]
         else if
